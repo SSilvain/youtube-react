704092abedcf77706842512d89c1cb7b99936d53
Silvain <silver383@gmail.com>
2021-08-06 00:26:01 +0300

    key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,5 @@
+import { KEY } from "./key";
 import axios from 'axios';
-const KEY = 'AIzaSyAFUNYmE1gfydRFrlb3Q05gXlPSgQmiY6I';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
